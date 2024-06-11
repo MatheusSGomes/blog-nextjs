36142c7eeab8c64e85d45f537fe5a068215b5ae5
Matheus Gomes <math.s.gomes@gmail.com>
2024-06-10 23:13:17 -0300

    feat: add handle post

--- pages/api/posts/index.ts
+++ pages/api/posts/index.ts
@@ -1,8 +1,10 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import axios from 'axios';
 
+// http://localhost:3002/posts
+
 const PostsApis = axios.create({
-    baseURL: 'http://localhost:3002/api',
+    baseURL: 'http://localhost:3002',
     headers: {
         'Content-Type': 'application/json',
     }
@@ -13,6 +15,9 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         case 'GET':
             return await handleGet(req, res);
 
+        case 'POST':
+            return await handlePost(req, res);
+
         default:
             return res.status(405).json({ error: 'Method not allowed' })
     }
@@ -22,6 +27,18 @@ async function handleGet(req: NextApiRequest, res: NextApiResponse) {
     try {
         const posts = await PostsApis.get('/posts');
         return res.status(200).json(posts.data);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function handlePost(req: NextApiRequest, res: NextApiResponse) {
+    try {
+        const newPost = req.body.data;
+
+        const createdPost = await PostsApis.post('/posts', newPost);
+
+        return res.status(200).json(createdPost.data);
     } catch (error) {
         console.log(error);
     }
