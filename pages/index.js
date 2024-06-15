bf8912901c157cba3d1eae29c2409c350eef93cb
Matheus Gomes <math.s.gomes@gmail.com>
2024-06-14 23:38:49 -0300

    style: navbar home

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import useIndex from '../data/hooks/pages/useIndex.page';
 import PostList from '../ui/components/PostList/PostList';
 import Head from 'next/head';
+import { Navbar, Typography } from '@material-tailwind/react';
 
 export default function Home() {
   const { posts } = useIndex();
@@ -12,10 +13,20 @@ export default function Home() {
         <meta name="meu blog" content="meu blog com excelentes posts"></meta>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <h1 className="text-2xl font-bold">
-          Hello world!
-        </h1>
+      <main class="mx-auto max-w-screen-xl">
+        <Navbar className="px-4 py-3 my-5 border-none" color="white">
+          <div className="flex items-center justify-center gap-y-4">
+            <Typography
+              as="a"
+              href="#"
+              variant="h3"
+              className="mr-4 ml-2 cursor-pointer py-1.5 text-black"
+            >
+              Meu Blog
+            </Typography>
+          </div>
+        </Navbar>
+
         <div className="grid grid-cols-3 gap-5">
           {posts.map((post) => (
             <PostList post={post} />
