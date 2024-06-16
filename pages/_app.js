d269b9b82d5d42474640019f4e9b9ce74fe0ecf8
Matheus Gomes <math.s.gomes@gmail.com>
2024-06-16 17:48:05 -0300

    feat: link logo

--- pages/_app.js
+++ pages/_app.js
@@ -15,9 +15,7 @@ function MyApp({ Component, pageProps }) {
       <main class="px-5 lg:py-0 mx-auto max-w-screen-lg">
         <div className="p-4 mt-5 mb-10 border-none rounded-xl bg-white">
           <div className="flex items-center justify-between">
-            <Typography as="a" href="/" variant="h3" className="mr-4 ml-2 cursor-pointer py-1.5 text-black">
-              Meu Blog
-            </Typography>
+            <Link class="text-black text-2xl font-medium" href="/">Meu Blog</Link>
 
             <div className="flex flex-row gap-2">
               <Link class="text-white bg-black p-2 rounded-md" href="/">Meus posts</Link>
