fa854ebfea5651b051d500362e9f35e3d3b9bdc5
Matheus Gomes <math.s.gomes@gmail.com>
2024-06-15 23:52:13 -0300

    feat: link to new post blog

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 import Head from 'next/head';
 import { Typography } from '@material-tailwind/react';
+import Link from 'next/link';
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -11,16 +12,13 @@ function MyApp({ Component, pageProps }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main class="px-5 lg:py-0 mx-auto max-w-screen-lg">
-        <div className="px-4 py-3 my-5 border-none rounded-xl bg-white">
-          <div className="flex items-center justify-center gap-y-4">
-            <Typography
-              as="a"
-              href="/"
-              variant="h3"
-              className="mr-4 ml-2 cursor-pointer py-1.5 text-black"
-            >
+        <div className="p-4 mt-5 mb-10 border-none rounded-xl bg-white">
+          <div className="flex items-center justify-between">
+            <Typography as="a" href="/" variant="h3" className="mr-4 ml-2 cursor-pointer py-1.5 text-black">
               Meu Blog
             </Typography>
+
+            <Link class="text-white bg-black p-2 rounded-md" href="posts/publish">Novo post</Link>
           </div>
         </div>
 
