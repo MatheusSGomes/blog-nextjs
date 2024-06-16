132dc3063b31c9fbde093e048e9ade0453a26baf
Matheus Gomes <math.s.gomes@gmail.com>
2024-06-15 23:05:52 -0300

    fix: api url

--- data/data/services/ApiService.ts
+++ data/data/services/ApiService.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // json-server db.json --port=3002 --watch
 // const url = process.env.URL_API;
-const url = "http://localhost:3002/posts";
+const url = "http://localhost:3002/";
 
 export const ApiService = axios.create({
     baseURL: url,
