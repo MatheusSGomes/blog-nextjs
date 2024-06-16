687d7860f9c6bb1e044ca6a73c8329f93b27cd78
Matheus Gomes <math.s.gomes@gmail.com>
2024-06-15 23:42:00 -0300

    feat: dangerously inner html

--- ui/components/PostBody/PostBody.tsx
+++ ui/components/PostBody/PostBody.tsx
@@ -1,11 +1,17 @@
 export default function PostBody({ post }) {
+
+  const markup = { __html: post.content };
+
   return (
     <div className="flex flex-col gap-5">
       <h1 className="text-4xl font-semibold text-center">{ post.title }</h1>
 
       <img className="w-full rounded-xl" src={ post.picture } alt={ post.title } />
 
-      <article className="text-xl">{ post.content }</article>
+      <div
+        className="text-xl"
+        dangerouslySetInnerHTML={ markup }
+      />
     </div>
   );
 }
