9404f05bdc192eba6f5805bcf0325fd266cb8c44
Matheus Gomes <math.s.gomes@gmail.com>
2024-06-15 23:06:35 -0300

    style: change main style

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,33 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import Head from 'next/head';
+import { Typography } from '@material-tailwind/react';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Head>
+        <title>Meu Blog</title>
+        <meta name="meu blog" content="meu blog com excelentes posts"></meta>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main class="mx-auto max-w-screen-xl">
+        <div className="px-4 py-3 my-5 border-none rounded-xl bg-white">
+          <div className="flex items-center justify-center gap-y-4">
+            <Typography
+              as="a"
+              href="#"
+              variant="h3"
+              className="mr-4 ml-2 cursor-pointer py-1.5 text-black"
+            >
+              Meu Blog
+            </Typography>
+          </div>
+        </div>
+
+        <Component {...pageProps} />
+      </main>
+    </div>
+  );
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -8,31 +8,11 @@ export default function Home() {
 
   return (
     <div>
-      <Head>
-        <title>Meu Blog</title>
-        <meta name="meu blog" content="meu blog com excelentes posts"></meta>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main class="mx-auto max-w-screen-xl">
-        <div className="px-4 py-3 my-5 border-none rounded-xl bg-white">
-          <div className="flex items-center justify-center gap-y-4">
-            <Typography
-              as="a"
-              href="#"
-              variant="h3"
-              className="mr-4 ml-2 cursor-pointer py-1.5 text-black"
-            >
-              Meu Blog
-            </Typography>
-          </div>
-        </div>
-
         <div className="grid grid-cols-3 gap-5">
           {posts.map((post) => (
             <PostList key={post.id} post={post} />
           ))}
         </div>
-      </main>
     </div>
   )
 }
